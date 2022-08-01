19f71132505f56422c7aef7c3a9f41e49a75fd85
Rezagholamif <103343986+Rezagholamif@users.noreply.github.com>
2022-08-01 08:22:15 +0330

    add uv detail view

--- uvs/urls.py
+++ uvs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import UvListView
+from .views import UvListView, UvDetailView
 
 urlpatterns = [
-    path('', UvListView.as_view(), name='uv_list')
+    path('', UvListView.as_view(), name='uv_list'),
+    path('<int:pk>/', UvDetailView.as_view(), name='uv_detail'),
 ]
--- uvs/views.py
+++ uvs/views.py
@@ -8,3 +8,7 @@ class UvListView(generic.ListView):
     template_name = 'uvs/uv_list.html'
     context_object_name = 'uvs'
 
+
+class UvDetailView(generic.DetailView):
+    model = Uv
+    template_name = 'uvs/uv_detail.html'
