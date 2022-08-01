92f86d8d8c7b9601ec445ba7452758cebc063518
Rezagholamif <103343986+Rezagholamif@users.noreply.github.com>
2022-08-01 06:54:19 +0330

    add Uv model

--- uvs/admin.py
+++ uvs/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Uv
 
-# Register your models here.
+admin.site.register(Uv)
--- uvs/models.py
+++ uvs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Uv(models.Model):
+    percent = models.PositiveIntegerField()
+    price = models.IntegerField()
+    brand = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.brand
+
+    def get_absolute_url(self):
+        return reverse('uv_detail', args=[self.id])
